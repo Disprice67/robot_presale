22c5c6846c1034135cd22a5e79c353ebf339be0b
Дмитрий Уткин <dimon965316@yandex.ru>
2024-11-24 21:27:15 +0300

    add .gitkeep

--- settings/config.py
+++ settings/config.py
@@ -5,7 +5,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 BUFFER_DIR = BASE_DIR / 'infrastructure/folders/buffer'
-NETWORK_DISK = Path('Z:\Робот')
+NETWORK_DISK = BASE_DIR / 'infrastructure/folders/network_disk'
 LOG_FILE = BASE_DIR / 'settings/robot.log'
 
 ENV = os.path.join(os.path.dirname(__file__), ".env")
